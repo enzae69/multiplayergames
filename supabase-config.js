27636e1e5e329421d6af40485ff774b6aec0abe6
enzae69 <enzaeenzae5@gmail.com>
2026-06-02 17:56:02 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = "https://ozvyymrqibpyqccgszlz.supabase.co";
-export const SUPABASE_ANON_KEY = "https://ozvyymrqibpyqccgszlz.supabase.co/rest/v1/";
+export const SUPABASE_ANON_KEY = "sb_publishable_30WmZF8d8h_rUSrTGtBwnQ_Kn9gquwL";
 
